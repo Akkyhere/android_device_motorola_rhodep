b654ec60a62a313b9834c5e90f01c217362ff917
Akshay Sharma <itsakkysharma@gmail.com>
2025-11-30 09:35:41 +0000

    rhodep: Add Moto G82 XT2225-1 overlays
    - Add framework overlay gated on ro.boot.hardware.sku=XT2225-1
    - Provide MMS user agent and power profile tuning for XT2225-1
    - Add Wi-Fi overlay for XT2225-1 with Motorola tethering SSID
    - Wire XT2225-1 overlay modules into product overlay packages
    
    Tested-on: Moto G82 (rhodep, XT2225-1)

--- device.mk
+++ device.mk
@@ -24,10 +24,12 @@ $(call inherit-product, device/motorola/sm6375-common/common.mk)
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResDevice \
+    FrameworksResrhodepXT2225-1 \
     LineageApertureAppDevice \
     LineageSystemUIDevice \
     SystemUIResDevice \
-    WifiResDevice
+    WifiResDevice \
+    WifiResRhodepXT2225-1
 
 # Audio
 PRODUCT_PACKAGES += \
